c2652f08b8d46325cf8c2716988b4a57424a4215
fredck <f.knabben@cksource.com>
2013-06-17 17:42:20 +0200

    Introduce the Image Caption plugin for Drupal.

---
+++ plugins/drupal_imagecaption/dev/theme.js
@@ -0,0 +1,97 @@
+// This is sample code for a theoretical theme, which upcasts <img> tags
+// to <figure> elements.
+
+// To enable this, just add ?theme to the URL.
+
+if ( document.location.search == '?theme' ) {
+
+	// Added this here for simplicity, but this is a sample CSS to be added
+	// by the theme into the editor contents area (contents.css).
+	CKEDITOR.addCss( '\
+				figure.imagecaption {\
+					border: solid 1px #ccc;\
+					border-radius: 2px;\
+					background: rgba(0,0,0,0.05);\
+					padding: 10px;\
+					margin: 10px 20px;\
+					text-align: center;\
+				}\
+				figure.imagecaption img{\
+					vertical-align: bottom;\
+				}\
+				figure.imagecaption figcaption {\
+					margin: 5px 10px -5px;\
+				}' );
+
+	CKEDITOR.on( 'instanceCreated', function( ev ) {
+		var editor = ev.editor;
+
+		// Listen to widget definitions and customize them as needed. It's
+		// basically rewritting parts of the definition.
+		editor.on( 'widgetDefinition', function( ev ) {
+			var widgetDef = ev.data;
+
+			// Customize the "imagecaption" widget definition.
+			if ( widgetDef.name == 'imagecaption' ) {
+
+				// Define the editables created by the new upcasting.
+				widgetDef.editables = {
+					caption: 'figcaption'
+				};
+
+				// Define the parts created by the new upcasting.
+				widgetDef.parts.caption = 'figcaption';
+
+				// Override "data" so we can make the new widget structure
+				// behave according to changes on data.
+				widgetDef.data = CKEDITOR.tools.override( widgetDef.data, function( originalDataFn ) {
+					return function() {
+						// Call the original "data" implementation.
+						originalDataFn.apply( this, arguments );
+
+						// Set the caption visibility.
+						this.parts.caption.setStyle( 'display', this.data.nocaption ? 'none' : '' );
+					};
+				} );
+
+				// Upcast <img> to <figure>.
+				widgetDef.upcast = CKEDITOR.tools.override( widgetDef.upcast, function( originalUpcastFn ) {
+					return function( el ) {
+						// Execute the original upcast first. If "true", this is an
+						// element to be upcasted.
+						if ( originalUpcastFn.apply( this, arguments ) ) {
+							var figure = el.wrapWith( new CKEDITOR.htmlParser.element( 'figure', { 'class': 'imagecaption' } ) ),
+								captionTxt = el.attributes[ 'data-caption' ] || '',
+								caption = CKEDITOR.htmlParser.fragment.fromHtml( captionTxt, 'figcaption' );
+
+							if ( !captionTxt )
+								caption.attributes.style = 'display:none';
+
+							figure.add( caption );
+
+							return figure;
+						}
+					}
+				} );
+
+				// Downcast <figure> back to <img>.
+				widgetDef.downcast = CKEDITOR.tools.override( widgetDef.downcast, function( originalDowncastFn ) {
+					return function( el ) {
+						// Update data with the current caption.
+						var caption = el.getFirst( 'figcaption' );
+						caption = caption ? caption.getHtml() : '';
+						this.data.caption = caption;
+
+						// When downcasting, we'll take the <img> element in
+						// consideration, only.
+						el = el.getFirst( 'img' );
+
+						// Call the original downcast to setup the <img>
+						// meta data accordingly.
+						return originalDowncastFn.call( this, el ) || el;
+					};
+				} );
+			}
+		} );
+	} );
+}
---
+++ plugins/drupal_imagecaption/plugin.js
@@ -0,0 +1,183 @@
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+'use strict';
+
+CKEDITOR.plugins.add( 'drupal_imagecaption', {
+	// Requires the "widget" plugin, which provides the Widgets System API.
+	requires: 'widget',
+
+	init: function( editor ) {
+		// Register the widget with a unique name "imagecaption".
+		editor.widgets.add( 'imagecaption', {
+			// ACF rules.
+			allowedContent: 'img[!src,data-align,data-caption]{float}',
+
+			parts: {
+				image: 'img'
+			},
+
+			// Initialization method called for every widget instance being
+			// upcasted.
+			init: function() {
+				var image = this.parts.image;
+
+				// Save the initial widget data.
+				this.setData( {
+					src: image.getAttribute( 'src' ),
+					caption: image.getAttribute( 'data-caption' ),
+					nocaption: !image.getAttribute( 'data-caption' ),
+					align: image.getAttribute( 'data-align' )
+				} );
+
+				// Remove the original element attributes.
+				image.removeAttributes( [ 'data-caption', 'data-align' ] );
+				image.removeStyle( 'float' );
+			},
+
+			// Called after initialization and on "data" changes.
+			data: function() {
+				// Set the "on editing" representation of the widget, which
+				// doesn't need to match the input/output data format.
+				this.parts.image.setAttribute( 'src', this.data.src );
+
+				if ( this.data.align == 'center' ) {
+					// Centered images require different styling, where text
+					// doesn't wrap around them.
+					this.wrapper.setStyle( 'float', 'none' );
+					this.wrapper.setStyle( 'text-align', 'center' );
+				} else {
+					this.wrapper.setStyle( 'float', this.data.align );
+					this.wrapper.removeStyle( 'text-align' );
+				}
+			},
+
+			// Check the elements that need to be converted to widgets.
+			upcast: function( el ) {
+				// Upcast all <img> elements that are alone inside a block
+				// element. That happens because our <img> are being placed
+				// in an invalid location and therefore CKEditor surrounds
+				// them with a <p>.
+				if ( el.name == 'img' ) {
+					if ( CKEDITOR.dtd.$block[ el.parent.name ] && el.parent.children.length == 1 )
+						return true;
+				}
+			},
+
+			// Convert the element back to its desired output representation.
+			downcast: function( el ) {
+				if ( this.data.caption )
+					el.attributes[ 'data-caption' ] = this.data.caption;
+
+				if ( this.data.align )
+					el.attributes[ 'data-align' ] = this.data.align;
+			}
+		} );
+
+		// All remaining <img> elements will still be transformed into
+		// Widgets, but those will be inline.
+		editor.widgets.add( 'imagecaptioninline', {
+			// This is an "inline" widget. It defaults to "block".
+			inline: true,
+
+			// ACF rules.
+			allowedContent: 'img[!src]{float}',
+
+			// Initialization method called for every widget instance being
+			// upcasted.
+			init: function() {
+				// Save the initial widget data.
+				this.setData( {
+					src: this.element.getAttribute( 'src' ),
+					align: this.element.getStyle( 'float' )
+				} );
+
+				// Remove the original element attributes.
+				this.element.removeStyle( 'float' );
+			},
+
+			// Called after initialization and on "data" changes.
+			data: function() {
+				// Set the "on editing" representation of the widget, which
+				// doesn't need to match the input/output data format.
+				this.element.setAttribute( 'src', this.data.src );
+
+				this.wrapper.setStyle( 'float', this.data.align );
+			},
+
+			// Check the elements that need to be converted to widgets.
+			upcast: function( el ) {
+				// Upcast all <img> elements that are alone inside a block
+				// element. That happens because our <img> are being placed
+				// in an invalid location and therefore CKEditor surrounds
+				// them with a <p>.
+				if ( el.name == 'img' ) {
+					if ( !CKEDITOR.dtd.$block[ el.parent.name ] || el.parent.children.length > 1 )
+						return true;
+				}
+			},
+
+			// Convert the element back to its desired output representation.
+			downcast: function( el, widget ) {
+				if ( this.data.align )
+					el.setStyle( 'data-align', this.data.align );
+			}
+		} );
+	},
+
+	afterInit: function( editor ) {
+		// Customize the behavior of the alignment commands.
+		setupAlignCommand( 'left' );
+		setupAlignCommand( 'right' );
+		setupAlignCommand( 'center' );
+		setupAlignCommand( 'block' );
+
+		function setupAlignCommand( value ) {
+			var command = editor.getCommand( 'justify' + value );
+			if ( command ) {
+				if ( value in { right:1,left:1,center:1 } ) {
+					command.on( 'exec', function( evt ) {
+						var widget = getSelectedWidget( editor ),
+							align;
+
+						if ( widget && ( widget.name == 'imagecaption' || value != 'center' ) ) {
+							widget.setData( { align: value } );
+							evt.cancel();
+						}
+					});
+				}
+
+				command.on( 'refresh', function( evt ) {
+					var widget = getSelectedWidget( editor ),
+						allowed = { right:1,left:1 },
+						align;
+
+					if ( widget ) {
+						align = widget.data.align;
+
+						// The inline widget doesn't accept center alignment,
+						// only the block one.
+						if ( widget.name == 'imagecaption' )
+							allowed.center = 1;
+
+						this.setState(
+							( align == value ) ? CKEDITOR.TRISTATE_ON : ( value in allowed ) ? CKEDITOR.TRISTATE_OFF : CKEDITOR.TRISTATE_DISABLED );
+
+						evt.cancel();
+					}
+				});
+			}
+		}
+
+		function getSelectedWidget( editor ) {
+			var widget = editor.widgets.focused;
+
+			if ( widget && widget.name in { imagecaption:1,imagecaptioninline:1 } )
+				return widget;
+
+			return null;
+		}
+	}
+} );
